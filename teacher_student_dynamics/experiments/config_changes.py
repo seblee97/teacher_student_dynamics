7304b0fe4bbcbc0a6ef041e691b70a211647c4a0
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-03-26 23:45:25 +0100

    initial commit fo config changes

---
+++ teacher_student_dynamics/experiments/config_changes.py
@@ -0,0 +1,11 @@
+import itertools
+
+param_config_changes = {
+    f"param_{h}": {"networks": {"student_hidden": h, "teacher_hidden": h}}
+    for h in [1, 2, 4]
+}
+oparam_config_changes = {
+    f"oparam_{h}": {"networks": {"student_hidden": 2 * h, "teacher_hidden": h}}
+    for h in [1, 2, 4]
+}
+CONFIG_CHANGES = {**param_config_changes, **oparam_config_changes}
