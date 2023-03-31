ffc58d571bfc9e194d4087379164ea8b9f22175a
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-03-31 14:47:49 +0100

    initial commit of alternate config changes files

---
+++ teacher_student_dynamics/experiments/config_changes_impaired_units.py
@@ -0,0 +1,23 @@
+import itertools
+
+param_config_changes = {
+    f"param_{h}": [
+        {"networks": {"student_hidden": h, "teacher_hidden": h}},
+        {"training": {"freeze_units": [f, 0]}},
+        {"curriculum": {"switch_steps": [s]}},
+    ]
+    for h, f, s in itertools.product(
+        [1, 2, 4], [1, 2, 3, 4], [1000, 10000, 100000, 1000000, 10000000]
+    )
+}
+oparam_config_changes = {
+    f"oparam_{h}": [
+        {"networks": {"student_hidden": 2 * h, "teacher_hidden": h}},
+        {"training": {"freeze_units": [f, 0]}},
+        {"curriculum": {"switch_steps": [s]}},
+    ]
+    for h, f, s in itertools.product(
+        [1, 2, 4], [1, 2, 3, 4], [1000, 10000, 100000, 1000000, 10000000]
+    )
+}
+CONFIG_CHANGES = {**param_config_changes, **oparam_config_changes}
