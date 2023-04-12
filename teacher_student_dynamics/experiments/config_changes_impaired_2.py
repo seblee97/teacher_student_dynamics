e9b74f6813ab4bc9271b39c534bfe4f9c349d8e7
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-04-12 19:20:07 +0100

    comma removed typo

--- teacher_student_dynamics/experiments/config_changes_impaired_2.py
+++ teacher_student_dynamics/experiments/config_changes_impaired_2.py
@@ -5,6 +5,5 @@ CONFIG_CHANGES = {
         {"networks": {"student_hidden": 4, "teacher_hidden": 2}},
         {"training": {"freeze_units": [2, 0]}},
         {"curriculum": {"switch_steps": [duration]}},
-    ],
-    for duration in [0, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]
+    ] for duration in [0, 1000, 2000, 5000, 10000, 20000, 50000, 100000, 200000, 500000, 1000000]
 }
