49a4c747ae78224b8f221202d8c338f82ea65cad
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-04-20 15:42:21 +0100

    initial commit of convergence speed config changes

---
+++ teacher_student_dynamics/experiments/config_changes_convergence_speed.py
@@ -0,0 +1,7 @@
+import itertools
+
+CONFIG_CHANGES = {
+    f"convergence_{th}_{sh}": [
+        {"networks": {"student_hidden": sh, "teacher_hidden": th}},
+    ] for (sh, th) in itertools.product([1,2,4,8,16], [1,2,3,4])
+}
