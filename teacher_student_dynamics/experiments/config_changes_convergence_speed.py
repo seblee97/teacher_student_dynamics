97fc2eab2f0532d6d27e20447383e5c354649996
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-05-03 17:40:16 +0100

    one fewer

--- teacher_student_dynamics/experiments/config_changes_convergence_speed.py
+++ teacher_student_dynamics/experiments/config_changes_convergence_speed.py
@@ -3,5 +3,5 @@ import itertools
 CONFIG_CHANGES = {
     f"convergence_{th}_{sh}": [
         {"networks": {"student_hidden": sh, "teacher_hidden": th}},
-    ] for (sh, th) in itertools.product([1, 2, 4, 8, 16, 32], [1, 2, 3, 4])
+    ] for (sh, th) in itertools.product([1, 2, 4, 8, 16, 32], [1, 2, 3])
 }
