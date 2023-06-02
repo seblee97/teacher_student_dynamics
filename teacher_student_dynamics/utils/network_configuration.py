ed14c2838fec4d2e63e3569c423a8a4ac56a73a9
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-06-02 21:09:08 +0100

    setter methods

--- teacher_student_dynamics/utils/network_configuration.py
+++ teacher_student_dynamics/utils/network_configuration.py
@@ -97,6 +97,10 @@ class VanillaNetworkConfiguration:
     def student_head_weights(self) -> List[np.ndarray]:
         return self._student_head_weights
 
+    @student_head_weights.setter
+    def student_head_weights(self, student_head_weights) -> None:
+        self._student_head_weights = student_head_weights
+
     @property
     def teacher_head_weights(self) -> List[np.ndarray]:
         return self._teacher_head_weights
@@ -104,6 +108,10 @@ class VanillaNetworkConfiguration:
     @property
     def student_self_overlap(self) -> np.ndarray:
         return self._student_self_overlap
+
+    @student_self_overlap.setter
+    def student_self_overlap(self, student_self_overlap) -> None:
+        self._student_self_overlap = student_self_overlap
 
     @property
     def teacher_self_overlaps(self) -> List[np.ndarray]:
@@ -116,6 +124,10 @@ class VanillaNetworkConfiguration:
     @property
     def student_teacher_overlaps(self) -> List[np.ndarray]:
         return self._student_teacher_overlaps
+
+    @student_teacher_overlaps.setter
+    def student_teacher_overlaps(self, student_teacher_overlaps) -> None:
+        self._student_teacher_overlaps = student_teacher_overlaps
 
     @property
     def configuration_dictionary(self):
@@ -155,7 +167,7 @@ class VanillaNetworkConfiguration:
         return network_configuration_dictionary
 
 
-class HiddenManifoldNetworkConfiguration:
+class HiddenManifoldNetworkConfiguration(VanillaNetworkConfiguration):
     """object to store configuration of student/teacher networks in unified way."""
 
     def __init__(
@@ -170,161 +182,62 @@ class HiddenManifoldNetworkConfiguration:
         student_local_field_covariances: List[np.ndarray],
         student_weighted_feature_matrix_self_overlaps: List[np.ndarray],
         feature_matrix_overlaps: List[np.ndarray],
+        student_teacher_overlap_densities: List[np.ndarray],
     ):
-        self._student_head_weights = student_head_weights
-        self._teacher_head_weights = teacher_head_weights
-        self._student_self_overlap = student_self_overlap
-        self._teacher_self_overlaps = teacher_self_overlaps
-        self._teacher_cross_overlaps = teacher_cross_overlaps
-        self._student_teacher_overlaps = student_teacher_overlaps
+
+        super().__init__(
+            student_head_weights=student_head_weights,
+            teacher_head_weights=teacher_head_weights,
+            student_self_overlap=student_self_overlap,
+            teacher_self_overlaps=teacher_self_overlaps,
+            teacher_cross_overlaps=teacher_cross_overlaps,
+            student_teacher_overlaps=student_teacher_overlaps,
+        )
+
         self._student_weighted_feature_matrices = student_weighted_feature_matrices
         self._student_local_field_covariances = student_local_field_covariances
         self._student_weighted_feature_matrix_self_overlaps = (
             student_weighted_feature_matrix_self_overlaps
         )
         self._feature_matrix_overlaps = feature_matrix_overlaps
-
-        self._num_teachers = len(self._teacher_head_weights)
-
-    def save(self, path: str) -> Dict[str, str]:
-
-        save_path_map = {}
-
-        np.savetxt(
-            os.path.join(path, f"{constants.STUDENT_SELF_OVERLAP}.csv"),
-            self._student_self_overlap,
-            delimiter=",",
-        )
-        save_path_map[constants.STUDENT_SELF_OVERLAP] = os.path.join(
-            path, f"{constants.STUDENT_SELF_OVERLAP}.csv"
-        )
-        for i, student_teacher_overlap in enumerate(self._student_teacher_overlaps):
-            op_name = f"{constants.STUDENT_TEACHER_OVERLAPS}_{i}"
-            save_path = os.path.join(path, f"{op_name}.csv")
-            np.savetxt(
-                save_path,
-                student_teacher_overlap,
-                delimiter=",",
-            )
-            save_path_map[op_name] = save_path
-        for i, student_head in enumerate(self._student_head_weights):
-            op_name = f"{constants.STUDENT_HEAD_WEIGHTS}_{i}"
-            save_path = os.path.join(path, f"{op_name}.csv")
-            np.savetxt(
-                save_path,
-                student_head,
-                delimiter=",",
-            )
-            save_path_map[op_name] = save_path
-        for i in range(self._num_teachers):
-            op_name = f"{constants.TEACHER_HEAD_WEIGHTS}_{i}"
-            save_path = os.path.join(path, f"{op_name}.csv")
-            np.savetxt(
-                os.path.join(path, f"{constants.TEACHER_HEAD_WEIGHTS}_{i}.csv"),
-                self._teacher_head_weights[i],
-                delimiter=",",
-            )
-            save_path_map[op_name] = save_path
-            op_name = f"{constants.TEACHER_SELF_OVERLAPS}_{i}"
-            save_path = os.path.join(path, f"{op_name}.csv")
-            np.savetxt(
-                os.path.join(path, f"{constants.TEACHER_SELF_OVERLAPS}_{i}.csv"),
-                self._teacher_self_overlaps[i],
-                delimiter=",",
-            )
-            save_path_map[op_name] = save_path
-
-        access_index = 0
-        for i in range(self._num_teachers):
-            for j in range(i, self._num_teachers):
-                if i != j:
-                    op_name = f"{constants.TEACHER_CROSS_OVERLAPS}_{i}_{j}"
-                    save_path = os.path.join(path, f"{op_name}.csv")
-                    np.savetxt(
-                        os.path.join(
-                            path, f"{constants.TEACHER_CROSS_OVERLAPS}_{i}_{j}.csv"
-                        ),
-                        self._teacher_cross_overlaps[access_index],
-                        delimiter=",",
-                    )
-                    save_path_map[op_name] = save_path
-                    access_index += 1
-
-        return save_path_map
+        self._student_teacher_overlap_densities = student_teacher_overlap_densities
 
     @property
-    def student_head_weights(self) -> List[np.ndarray]:
-        return self._student_head_weights
-
-    @property
-    def teacher_head_weights(self) -> List[np.ndarray]:
-        return self._teacher_head_weights
-
-    @property
-    def student_self_overlap(self) -> np.ndarray:
-        return self._student_self_overlap
-
-    @property
-    def student_weighted_feature_matrices(self) -> np.ndarray:
+    def student_weighted_feature_matrices(self) -> List[np.ndarray]:
         return self._student_weighted_feature_matrices
 
+    @student_weighted_feature_matrices.setter
+    def student_weighted_feature_matrices(
+        self, student_weighted_feature_matrices
+    ) -> np.ndarray:
+        self._student_weighted_feature_matrices = student_weighted_feature_matrices
+
     @property
-    def student_local_field_covariances(self) -> np.ndarray:
+    def student_local_field_covariances(self) -> List[np.ndarray]:
         return self._student_local_field_covariances
 
-    @property
-    def student_weighted_feature_matrix_self_overlaps(self) -> np.ndarray:
-        return self._student_weighted_feature_matrix_self_overlaps
+    @student_local_field_covariances.setter
+    def student_local_field_covariances(
+        self, student_local_field_covariances
+    ) -> List[np.ndarray]:
+        self._student_local_field_covariances = student_local_field_covariances
 
     @property
-    def feature_matrix_overlaps(self) -> np.ndarray:
+    def student_weighted_feature_matrix_self_overlaps(self) -> List[np.ndarray]:
+        return self._student_weighted_feature_matrix_self_overlaps
+
+    @student_weighted_feature_matrix_self_overlaps.setter
+    def student_weighted_feature_matrix_self_overlaps(
+        self, student_weighted_feature_matrix_self_overlaps
+    ) -> np.ndarray:
+        self._student_weighted_feature_matrix_self_overlaps = (
+            student_weighted_feature_matrix_self_overlaps
+        )
+
+    @property
+    def feature_matrix_overlaps(self) -> List[np.ndarray]:
         return self._feature_matrix_overlaps
 
     @property
-    def teacher_self_overlaps(self) -> List[np.ndarray]:
-        return self._teacher_self_overlaps
-
-    @property
-    def teacher_cross_overlaps(self) -> List[np.ndarray]:
-        return self._teacher_cross_overlaps
-
-    @property
-    def student_teacher_overlaps(self) -> List[np.ndarray]:
-        return self._student_teacher_overlaps
-
-    @property
-    def configuration_dictionary(self):
-        return {
-            constants.STUDENT_SELF_OVERLAP: self._student_self_overlap,
-            constants.STUDENT_TEACHER_OVERLAPS: self._student_teacher_overlaps,
-            constants.STUDENT_HEAD_WEIGHTS: self._student_head_weights,
-            constants.TEACHER_HEAD_WEIGHTS: self._teacher_head_weights,
-            constants.TEACHER_CROSS_OVERLAPS: self._teacher_cross_overlaps,
-            constants.TEACHER_SELF_OVERLAPS: self._teacher_self_overlaps,
-        }
-
-    @property
-    def sub_dictionary(self):
-        network_configuration_dictionary = {}
-
-        for i, head in enumerate(self._student_head_weights):
-            for j, weight in enumerate(head):
-                network_configuration_dictionary[
-                    f"{constants.STUDENT_HEAD}_{i}_{constants.WEIGHT}_{j}"
-                ] = weight
-        for i, head in enumerate(self._teacher_head_weights):
-            for j, weight in enumerate(head):
-                network_configuration_dictionary[
-                    f"{constants.TEACHER_HEAD}_{i}_{constants.WEIGHT}_{j}"
-                ] = weight
-        for (i, j), overlap_value in np.ndenumerate(self._student_self_overlap):
-            network_configuration_dictionary[
-                f"{constants.STUDENT_SELF_OVERLAP}_{i}_{j}"
-            ] = overlap_value
-        for t, student_teacher_overlap in enumerate(self._student_teacher_overlaps):
-            for (i, j), overlap_value in np.ndenumerate(student_teacher_overlap):
-                network_configuration_dictionary[
-                    f"{constants.STUDENT_TEACHER}_{t}_{constants.OVERLAP}_{i}_{j}"
-                ] = overlap_value
-
-        return network_configuration_dictionary
+    def student_teacher_overlap_densities(self) -> List[np.ndarray]:
+        return self._student_teacher_overlap_densities
