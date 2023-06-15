d7d0e1721be6890c21e5efc198f21844d43c70f6
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2023-06-15 15:35:37 +0100

    sigma 1 log map

--- teacher_student_dynamics/ode/c_ode/hmm/runner.cpp
+++ teacher_student_dynamics/ode/c_ode/hmm/runner.cpp
@@ -113,6 +113,7 @@ int main(int argc, char **argv)
 
     std::map<std::string, std::vector<double>> q_log_map;
     std::map<std::string, std::vector<double>> w_log_map;
+    std::map<std::string, std::vector<double>> sigma_1_log_map;
     std::map<std::string, std::vector<double>> r_log_map;
     std::map<std::string, std::vector<double>> u_log_map;
     std::map<std::string, std::vector<double>> h_0_log_map;
@@ -124,8 +125,10 @@ int main(int argc, char **argv)
         {
             std::vector<double> q_ij_log(num_logs);
             std::vector<double> w_ij_log(num_logs);
+            std::vector<double> sigma_1_ij_log(num_logs);
             q_log_map["q_" + std::to_string(i) + std::to_string(j)] = q_ij_log;
             w_log_map["w_" + std::to_string(i) + std::to_string(j)] = w_ij_log;
+            sigma_1_log_map["sigma_1_" + std::to_string(i) + std::to_string(j)] = sigma_1_ij_log;
         }
     }
 
@@ -181,6 +184,7 @@ int main(int argc, char **argv)
                 {
                     q_log_map["q_" + std::to_string(s) + std::to_string(s_)][log_i] = state.state["Q"](s, s_);
                     w_log_map["w_" + std::to_string(s) + std::to_string(s_)][log_i] = state.state["W"](s, s_);
+                    sigma_1_log_map["sigma_1_" + std::to_string(s) + std::to_string(s_)][log_i] = state.state["Sigma1"](s, s_);
                 }
             }
 
@@ -253,6 +257,17 @@ int main(int argc, char **argv)
             for (int n = 0; n < num_logs; n++)
             {
                 file << w_log_map["w_" + std::to_string(i) + std::to_string(j)][n];
+                if (n < num_logs - 1)
+                {
+                    file << "\n";
+                }
+            }
+            file.close();
+            csv_name = "sigma_1_" + std::to_string(i) + std::to_string(j) + ".csv";
+            file.open(output_path / csv_name);
+            for (int n = 0; n < num_logs; n++)
+            {
+                file << sigma_1_log_map["sigma_1_" + std::to_string(i) + std::to_string(j)][n];
                 if (n < num_logs - 1)
                 {
                     file << "\n";
