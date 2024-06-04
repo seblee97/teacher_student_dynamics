3988ce61826d80983cd300d3acc1ce1353e7ee48
Sebastian Lee <sebastianlee.1997@yahoo.co.uk>
2024-06-04 10:47:52 +0100

    F matrix is DxN

--- teacher_student_dynamics/runners/hmm_multi_teacher_runner.py
+++ teacher_student_dynamics/runners/hmm_multi_teacher_runner.py
@@ -534,7 +534,7 @@ class HMMMultiTeacherRunner(base_network_runner.BaseNetworkRunner):
             base_feature_matrix = torch.normal(
                 mean=0.0,
                 std=1.0,
-                size=(self._latent_dimension, self._latent_dimension),
+                size=(self._latent_dimension, self._input_dimension),
                 device=self._device,
             )
             data_modules = [
@@ -615,7 +615,7 @@ class HMMMultiTeacherRunner(base_network_runner.BaseNetworkRunner):
             base_feature_matrix = torch.normal(
                 mean=0.0,
                 std=1.0,
-                size=(self._latent_dimension, self._latent_dimension),
+                size=(self._latent_dimension, self._input_dimension),
                 device=self._device,
             )
             # Covariance matrix of base features
